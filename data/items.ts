a7cf5ca09b30637d56e09641a81d887ab037f39f
root <root@instance-1.europe-west1-b.c.true-velocity-291514.internal>
2020-10-30 16:00:30 +0000

    mega helio

--- data/items.ts
+++ data/items.ts
@@ -2356,6 +2356,20 @@ export const Items: {[itemid: string]: ItemData} = {
 		gen: 3,
 		isNonstandard: "Past",
 	},
+	heliolite: {
+		name: "Heliolite",
+		spritenum: 590,
+		megaStone: "Heliolisk-Mega",
+		megaEvolves: "Heliolisk",
+		itemUser: ["Heliolisk"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: 1412,
+		gen: 6,
+		isNonstandard: "Past",
+	},
 	heracronite: {
 		name: "Heracronite",
 		spritenum: 590,
